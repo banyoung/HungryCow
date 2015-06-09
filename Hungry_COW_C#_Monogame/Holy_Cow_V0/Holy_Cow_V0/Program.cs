9df430732c06e225af8246d5b09134a61b1c3ef2
pfuez <pstaeuble@hotmail.com>
2015-06-09 13:55:14 +0200

    Everything Works with Visual Studio

--- Hungry_COW_C#_Monogame/Holy_Cow_V0/Holy_Cow_V0/Program.cs
+++ Hungry_COW_C#_Monogame/Holy_Cow_V0/Holy_Cow_V0/Program.cs
@@ -14,7 +14,6 @@ namespace Holy_Cow_V0
         [STAThread]
         static void Main()
         {
-            Console.WriteLine("HelloGitWorld!");
             using (var game = new Game1())
                 game.Run();
         }
